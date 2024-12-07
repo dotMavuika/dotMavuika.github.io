786c5eed647dc2a5601bdba54bf64686bdcf3441
dotMavuika <jansebastian11@hotmail.com>
2024-12-07 13:40:22 -0500

    Integración de tailwind

---
+++ Nueva carpeta/docs/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	  content: [
+		      "./src/**/*.{html,ts}", // Analiza HTML y TypeScript en Angular
+		    ],
+	  theme: {
+		      extend: {}, // Aquí puedes personalizar tu tema
+		    },
+	  plugins: [],
+}
+
+
